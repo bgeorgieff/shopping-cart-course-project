7de6aada980e5c7332866fd0b13e1a9d93c8d3f8
bgeorgieff <b.g.georgiev87@gmail.com>
2022-03-31 10:20:53 +0300

    with routing

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shoppingList.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("Meat", 2), new Ingredient("Bread", 1)]
+    ),
+    new Recipe(
+      "Another Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("Corn", 1), new Ingredient("Yoghurt", 1)]
+    ),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(id: number) {
+    return this.recipes.slice()[id];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
